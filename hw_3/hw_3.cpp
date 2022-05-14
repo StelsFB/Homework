53da994f743a24c48b18e6c9c5fc7a6b381494b1
duniodiana <duniodiana@gmail.com>
2022-05-14 23:53:05 +0300

    lesson 4

---
+++ hw_3/hw_3.cpp
@@ -0,0 +1,39 @@
+﻿#include "locale.h"
+#include <stdio.h>
+
+
+void rec(int n)
+{
+	if (n != 0)
+		rec(n / 2);
+	else return;
+	printf("%d", n % 2);
+	return;
+}
+
+float power(float x, int y)
+{
+
+	int i;
+	float rezult = 1;
+
+	for (i = 1; i <= y; ++i)
+	{
+		rezult *= x;
+	}
+	printf("%f", rezult);
+	return 0;
+}
+int main()
+{
+	float n;
+	int m;
+	printf("Введите десятичное число : \n");
+	scanf("%f", &n);
+	rec(n);
+	printf("\n");
+	printf("степень : \n");
+	scanf("%d", &m);
+	power(n, m);
+	return 0;
+}
